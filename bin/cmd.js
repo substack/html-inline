2d83674b43d6475a818ccedb2f4e3d3568ae7fbd
James Halliday <mail@substack.net>
2014-11-02 23:07:58 -0800

    path juggling

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,7 +2,24 @@
 
 var inliner = require('../');
 var fs = require('fs');
+var path = require('path');
+var minimist = require('minimist');
 
-var inline = inliner({ basedir: process.cwd() });
-
-process.stdin.pipe(inline).pipe(process.stdout);
+var argv = minimist(process.argv.slice(2), {
+    alias: { i: 'infile', o: 'outfile', b: 'basedir' },
+    default: { outfile: '-' }
+});
+var infile = argv.infile || argv._[0];
+if (!argv.basedir && infile) {
+    argv.basedir = path.resolve(path.dirname(infile));
+}
+var inline = inliner(argv);
+var input = infile === '-' || !infile
+    ? process.stdin
+    : fs.createReadStream(infile)
+;
+var output = argv.outfile === '-'
+    ? process.stdout
+    : fs.createWriteStream(argv.outfile)
+;
+input.pipe(inline).pipe(output);
